8f2666afa8fe4c65663e6ed5551e0ec1f03451a2
Ivan Vazquez <ivanzur907@gmail.com>
2022-08-30 17:20:14 -0500

    Se sube el proyecto

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layaouts.plantilla')
+@section('contenido')
+<h1>Aqui va el carrucel de imagenes</h1>
+<img src="C:\xampp\htdocs\Laravel\proyectosa\public\imagenes\t1.jpg" class="img-fluid" alt="...">
+@stop 
